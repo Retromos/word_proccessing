fc411b12df11bf98c9c48bbe87e533e153145d40
wkubheka <wkubheka023@student.wethinkcode.co.za>
2023-11-19 15:13:10 +0200

    ccs

--- word_processing.py
+++ word_processing.py
@@ -2,48 +2,48 @@
 Beginning of psychological trauma
 """
 
+# text = open("words.txt", "r")
+# print(text.read())
+
 
 def get_words(file):
     """
-    function returns a list of all words from the read txt file
+    function returns a list of all words from the read txt file 
     args:: file - file directo
     returns:: a list of words...
     """
-    f = open("words.txt", "r")
-    words = f.read()
-    f.close()
-    return words
-
+    ...
 
 def cleaning_words(words):
-    """
+    '''
     function lowers all words
-    args:: words list
+    args:: words list 
     returns:: lowers words ready from processing
-    """
-
+    '''
+    ...
 
 def get_userinput():
-    """
+    '''
     take user input then filters by making sure its alphabet
-    """
-    ...
-
+    '''
+    user_input =  input('search for word: ')
+    while not user_input or not user_input.isalpha():
+        user_input =  input('search for word (enter a valid word): ')
+    return user_input
 
 def user_word_split(words):
-    """
+    '''
     if the user enters a space we assume the word is complete
-    args:: words list
+    args:: words list 
 
     returns the list of words
-    """
+    '''
     ...
 
-
 def user_word_count_sort(words):
-    """
+    '''
     sorts the list based on most typed words
-    args:: words list
+    args:: words list 
 
     returns the sorted list of most used words
     '''
@@ -58,4 +58,3 @@ def main():
         user = get_userinput()
 
 
-main
