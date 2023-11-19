e26b250328ec59f308e956c8e23cb9ee17184e45
wkubheka <wkubheka023@student.wethinkcode.co.za>
2023-11-19 14:45:37 +0200

    functions mos

--- word_processing.py
+++ word_processing.py
@@ -26,3 +26,28 @@ def cleaning_words(words):
     returns:: lowers words ready from processing
     '''
     ...
+
+def get_userinput():
+    '''
+    take user input then filters by making sure its alphabet
+    '''
+    ...
+
+def user_word_split(words):
+    '''
+    if the user enters a space we assume the word is complete
+    args:: words list 
+
+    returns the list of words
+    '''
+    ...
+
+def user_word_count_sort(words):
+    '''
+    sorts the list based on most typed words
+    args:: words list 
+
+    returns the sorted list of most used words
+
+    '''
+    ...
