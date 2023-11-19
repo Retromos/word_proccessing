02986bbdcaf9e509f53a5cd51a89bbe234bc2238
wkubheka <wkubheka023@student.wethinkcode.co.za>
2023-11-19 14:36:45 +0200

    comments

--- word_processing.py
+++ word_processing.py
@@ -1,1 +1,28 @@
+"""
+Beginning of psychological trauma
+"""
 
+
+
+
+text = open("words.txt", "r")
+print(text.read())
+
+
+def get_words(file):
+    """
+    function returns a list of all words from the read txt file 
+    args:: file - file directo
+    returns:: a list of words...
+
+
+    """
+    ...
+
+def cleaning_words(words):
+    '''
+    function lowers all words
+    args:: words list 
+    returns:: lowers words ready from processing
+    '''
+    ...
